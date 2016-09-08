22bffd7b1495516eacf41ccaf0f6dd7f9b7a10bc
strubix <lucas.strubi@gmail.com>
2016-09-08 15:53:13 +0200

    header-nav: logo, cart, drop menu

--- front-end/src/components/header-nav/header-nav.controller.js
+++ front-end/src/components/header-nav/header-nav.controller.js
@@ -1,2 +1,18 @@
 export default class HeaderNavController {
+  constructor() {
+    this.dropMenu = function(object) {
+      const icon = object.target.attributes[0];
+      if(icon.value == "img/drop.svg") {
+        this.console();
+        icon.value = "img/hamburger.svg";
+      } else {
+        icon.value = "img/drop.svg";
+      }
+    };
+
+    this.console = function (){
+      console.log('drop menu');
+    }
+  }
+
 }
