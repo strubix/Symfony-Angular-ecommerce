6cbd51f80c51a493d2a859d3d6bf61730b754c28
strubix <lucas.strubi@gmail.com>
2016-09-08 20:37:03 +0200

    footer

--- front-end/src/app.components.js
+++ front-end/src/app.components.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import Home from './components/home/home';
 import Navbar from './components/navbar/navbar';
 import HeaderNav from './components/header-nav/header-nav';
+import Footer from './components/footer-component/footer-component';
 
 const components = 'app.components';
 
@@ -9,6 +10,7 @@ angular.module(components, [])
     .component('home', Home)
     .component('navbar', Navbar)
     .component('headerNav', HeaderNav)
+    .component('footerComponent', Footer)
 ;
 
 export default components;
---
+++ front-end/src/components/footer-component/footer-component.js
@@ -0,0 +1,9 @@
+import controller from './footer-component.controller';
+import './footer-component.style.scss';
+
+export default {
+  templateUrl: 'components/footer-component/footer-component.template.html',
+  controller: controller,
+  controllerAs: 'vm',
+  bindings: {},
+};
